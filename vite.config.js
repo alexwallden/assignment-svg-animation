9e9b8b913ec66977cc39b9bd43a52a98b911e44f
alexwallden <alexander.wallden@me.com>
2023-01-19 19:48:38 +0100

    :tada: first commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/assignment-svg-animation/',
+  define: {
+    'process.env': {},
+  },
+});
+
+import { defineConfig } from 'vite';
